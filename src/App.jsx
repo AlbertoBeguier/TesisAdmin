4eca11e60ff3f7e8a87e83571dfbce72ff790175
Alberto Beguier <aabeguier@hotmail.com>
2024-09-09 19:04:33 -0300

    Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "../src/styles/App.scss";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Empresa } from "./pages/Empresa"; // Página de la Empresa
-import { Home } from "./pages/home"; // Página de inicio
+import { Home } from "./pages/Home"; // Página de inicio
 import { AnalisisEstrategico } from "./pages/AnalisisEstrategico"; // Página de Análisis Estratégico
 import { AnalisisSituacion } from "./pages/AnalisisSituacion"; // Página de Análisis de la Situación
 import { FodaForm } from "./components/FodaForm"; // Formulario FODA
